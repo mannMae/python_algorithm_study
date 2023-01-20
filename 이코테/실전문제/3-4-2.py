6657697bfec5d13982a3a3187927b1d409f8456f
김재만 <daga4242@gmail.com>
2023-01-20 12:41:37 +0900

    greedy

---
+++ 이코테/실전문제/3-4-2.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    target = n//k
+    result += n-target
+    n = target
+    
+    if n < k:
+        break
+    
+    result +=1
+    n //=k
+    
+result += n-1
